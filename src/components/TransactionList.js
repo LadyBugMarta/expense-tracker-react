d3c81d6600376f7dd74df397528973d4f9b7d793
Marta <martapabis.it@gmail.com>
2020-10-27 11:11:58 +0100

    income & expense

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Transaction } from './Transaction';
+
+import { GlobalContext } from '../context/GlobalState';
 
 export const TransactionList = () => {
+    const { transactions } = useContext(GlobalContext);
+
     return (
         <>
              <h3>History</h3>
-             <ul id="list" class="list">
-             <li className="minus">
-             Cash <span>-$400</span><button class="delete-btn">x</button>
-            </li>
+             <ul id="list" className="list">
+                 {transactions.map(transaction => (<Transaction key={Transaction.id} transaction={transaction}/>) )}
+             
       </ul>
             
         </>
