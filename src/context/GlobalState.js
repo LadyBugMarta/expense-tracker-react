6a408bbeea6f57a5c2797f008a2acee2196ba7f9
Marta <martapabis.it@gmail.com>
2020-10-27 11:54:31 +0100

    balance changes

--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -4,12 +4,7 @@ import AppReducer from './AppReducer';
 // Initial state
 
 const InitiialState = {
-    transactions: [
-          { id: 1, text: 'Flower', amount: -20 },
-          { id: 2, text: 'Salary', amount: 300 },
-          { id: 3, text: 'Book', amount: -10 },
-          { id: 4, text: 'Camera', amount: 150 }
-        ]
+    transactions: []
 } 
 
 // Create context
@@ -24,6 +19,12 @@ export const GlobalProvider = ({ children }) => {
         dispatch({
             type: 'DELETE_TRANSACTION',
             payload: id
+        })
+    }
+    function addTransaction(transaction){
+        dispatch({
+            type: 'ADD_TRANSACTION',
+            payload: transaction
         })
     }
 
